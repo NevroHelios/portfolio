d518433a93168d36cd77fa63f62f193b0978c3a4
NevroHelios <dasharka05@gmail.com>
2025-01-17 01:53:22 +0530

    Updated AppleCardsCarousel component in src/components/ui/apple-cards-carousel.tsx to improve functionality.

--- src/components/ui/apple-cards-carousel.tsx
+++ src/components/ui/apple-cards-carousel.tsx
@@ -18,7 +18,7 @@ import Image, { ImageProps } from "next/image";
 import { useOutsideClick } from "@/hooks/useOutsideClick";
 
 interface CarouselProps {
-  items: JSX.Element[];
+  // items: JSX.Element[];
   initialScroll?: number;
 }
 
